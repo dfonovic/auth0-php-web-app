75b5ee477943b96cf80c7300d64abc79953cbea3
dfonovic <40169496+dfonovic@users.noreply.github.com>
2020-04-01 18:38:50 +0200

    Update login.php

--- 00-Starter-Seed/login.php
+++ 00-Starter-Seed/login.php
@@ -1,13 +1,15 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/dotenv-loader.php';
+// login.php
 
-$auth0 = new Auth0\SDK\Auth0([
-    'domain' => getenv('AUTH0_DOMAIN'),
-    'client_id' => getenv('AUTH0_CLIENT_ID'),
-    'redirect_uri' => getenv('AUTH0_CALLBACK_URL'),
-    'audience' => getenv('AUTH0_AUDIENCE'),
-    'scope' => 'openid profile email',
+require 'vendor/autoload.php';
+use Auth0\SDK\Auth0;
+
+$auth0 = new Auth0([
+  'domain' => 'dev-70pwcc3g.eu.auth0.com',
+  'client_id' => 'jXA7M34py8iQfjgjMI3yeUIboL3spLDj',
+  'client_secret' => 'CGs-pkjetgqFhv1GEvFQBGQX_i3vXD2gmKG2h8R-Ocbyq-oV2jTqJfZLbnvfxeuZ',
+  'redirect_uri' => 'http://port-3001.php_learninig-dfdfonovic619477.codeanyapp.com/callback',
+  'scope' => 'openid profile email',
 ]);
 
 $auth0->login();
